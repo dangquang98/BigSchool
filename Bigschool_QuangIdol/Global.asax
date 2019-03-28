5053a80d1ba47256ff6a5d576781bf72402697fe
Administrator <Administrator@PC020>
2019-03-28 10:04:52 +0700

    Add project files.

---
+++ Bigschool_QuangIdol/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bigschool_QuangIdol.MvcApplication" Language="C#" %>
---
+++ Bigschool_QuangIdol/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bigschool_QuangIdol.Startup))]
+namespace Bigschool_QuangIdol
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
